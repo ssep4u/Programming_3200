3626195fc776b1be225cb4f84c54dbd791313b83
ssep <ssep4u@gmail.com>
2026-03-24 09:46:37 +0900

    counter HTML id, class, JS addEventListener(), onclick

---
+++ 02_counter/main.js
@@ -0,0 +1,31 @@
+let count = 0;
+
+// HTML -> JS
+// const resultH1 = document.querySelectorAll("h1")[0];
+// const resultH1 = document.querySelector("h1");
+// const resultH1 = document.getElementsByTagName("h1")[0];
+// const plusButton = document.getElementsByTagName("button")[0];
+const resultH1 = document.getElementById("result");
+// const resultH1 = document.querySelector("#result");
+// const resultH1 = resultH1;
+const plusButton = document.getElementsByClassName("plusBtn")[0];
+
+// plusButton.addEventListener("click", () => {
+//     // 숫자 증가하자
+//     count++;    //count += 1;   // count = count + 1;
+    
+//     // 숫자 표시하자
+//     resultH1.innerHTML = count;
+// });
+plusButton.onclick = () => {
+    // 숫자 증가하자
+    count++;    //count += 1;   // count = count + 1;
+    
+    // 숫자 표시하자
+    resultH1.innerHTML = count;
+};
+
+// (함수정의)();
+// (function () {
+
+// })()
