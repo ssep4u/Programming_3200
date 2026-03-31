9cdf72b1c803a76bea456c6f5a0dba389b7d64a1
ssep <ssep4u@gmail.com>
2026-03-31 09:46:16 +0900

    CouterApp Too many re-renders. 🙀

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -7,12 +7,15 @@ import heroImg from './assets/hero.png'
 function CounterApp() {
   //count: 이전 또는 현재 값. setCount(): count 값 변경 함수. 반드시 이걸 거쳐야 함
   const [count, setCount] = useState(0)
+  
   return (
     <>
       <h1>{count}</h1>
       <button onClick={() => setCount((count) => count + 1)}>+</button>
       <button onClick={() => setCount((count) => count + 2)}>+2</button>
       <button onClick={() => setCount((count) => count - 1)}>-</button>
+      <button onClick={() => setCount(0)}>reset</button>
+      {/* <button onClick={setCount(0)}>reset</button>  //Too many re-renders. */}
       {/* <button
         className="counter"
         onClick={() => setCount((count) => count + 1)}  // JS 함수호출X; 함수정의O
