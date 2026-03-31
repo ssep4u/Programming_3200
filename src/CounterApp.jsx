b8c81bf93b85da9ccb1bf05aa30b6cf35e515eb6
ssep <ssep4u@gmail.com>
2026-03-31 09:36:27 +0900

    CounterApp +

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -5,16 +5,18 @@ import heroImg from './assets/hero.png'
 // import './App.css'
 
 function CounterApp() {
+  //count: 이전 또는 현재 값. setCount(): count 값 변경 함수. 반드시 이걸 거쳐야 함
   const [count, setCount] = useState(0)
-
   return (
     <>
-      <button
+      <h1>{count}</h1>
+      <button onClick={() => setCount((count) => count + 1)}>+</button>
+      {/* <button
         className="counter"
-        onClick={() => setCount((count) => count + 1)}
+        onClick={() => setCount((count) => count + 1)}  // JS 함수호출X; 함수정의O
       >
         Count is {count}
-      </button>
+      </button> */}
     </>
   )
 }
