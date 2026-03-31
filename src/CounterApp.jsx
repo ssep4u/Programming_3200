d5956a073a692a9ac0dc8f2969ee14696775ebcd
ssep <ssep4u@gmail.com>
2026-03-31 09:59:07 +0900

    CounterApp +(최대 10까지), if문, 삼항연산자, Math.min()

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -7,7 +7,7 @@ import heroImg from './assets/hero.png'
 function CounterApp() {
   //count: 이전 또는 현재 값. setCount(): count 값 변경 함수. 반드시 이걸 거쳐야 함
   const [count, setCount] = useState(0)
-  
+
   return (
     <>
       <h1>{count}</h1>
@@ -15,6 +15,12 @@ function CounterApp() {
       <button onClick={() => setCount((count) => count + 2)}>+2</button>
       <button onClick={() => setCount((count) => count - 1)}>-</button>
       <button onClick={() => setCount(0)}>reset</button>
+      <button onClick={() => setCount((count) => {
+        if (count + 1 >= 10) return 10;
+          return count + 1;
+      })}>+(최대 10까지)</button>
+      <button onClick={() => setCount((count) => count + 1 >=10?10:count+1)}>+(최대 10까지)</button>
+      <button onClick={() => setCount((count) => Math.min(count + 1, 10))}>+(최대 10까지)</button>
       {/* <button onClick={setCount(0)}>reset</button>  //Too many re-renders. */}
       {/* <button
         className="counter"
