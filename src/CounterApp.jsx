b6f3c03a65e4c528c7b6c5a74d014b4fe702953d
ssep <ssep4u@gmail.com>
2026-04-02 13:29:35 +0900

    CounterApp plusMax10

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -7,6 +7,12 @@ import heroImg from './assets/hero.png'
 function CounterApp() {
   //count: 이전 또는 현재 값. setCount(): count 값 변경 함수. 반드시 이걸 거쳐야 함
   const [count, setCount] = useState(0)
+  function plusMax10() {
+    setCount((count) => {
+      if (count + 1 >= 10) return 10;
+      return count + 1;
+    })
+  }
 
   return (
     <>
@@ -17,10 +23,13 @@ function CounterApp() {
       <button onClick={() => setCount(0)}>reset</button>
       <button onClick={() => setCount((count) => {
         if (count + 1 >= 10) return 10;
-          return count + 1;
+        return count + 1;
       })}>+(최대 10까지)</button>
-      <button onClick={() => setCount((count) => count + 1 >=10?10:count+1)}>+(최대 10까지)</button>
+      <button onClick={() => setCount((count) => count + 1 >= 10 ? 10 : count + 1)}>+(최대 10까지)</button>
       <button onClick={() => setCount((count) => Math.min(count + 1, 10))}>+(최대 10까지)</button>
+      <button onClick={() => plusMax10()}>+(최대 10까지)</button>
+      <button onClick={plusMax10}>+(최대 10까지)</button>
+      {/* <button onClick={plusMax10()}>+(최대 10까지)</button> */}
       {/* <button onClick={setCount(0)}>reset</button>  //Too many re-renders. */}
       {/* <button
         className="counter"
