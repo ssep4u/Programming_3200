cacd1c493f8447463c0fd815997107a5f3cc7e38
jagseeun <s2468@e-mirim.hs.kr>
2026-05-26 09:34:12 +0900

    3214 장세은  3-1	추가할 때 맨 위에 표시 하자	★	addTodo() 수정

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -36,12 +36,13 @@ function TodoListApp() {
   function addTodo(text) {
     //이전 todos에 newTodo 만들어서 추가하자 -> 그것을 setTodos() 하자
     setTodos((todos) => [
-      ...todos,     //todos에 있는 item을 다 꺼내서 새로운 리스트에 하나씩 넣자
+      
       new Todo(
         Date.now(), //id: 고유 ID 시간을 이용. == new Date().getTime()
         text,       //text: 할 일 내용
         false       //isCompleted: 할 일의 완료 여부. 초기값은 false
-      )
+      ),
+      ...todos     //todos에 있는 item을 다 꺼내서 새로운 리스트에 하나씩 넣자
     ]);
   }
   function toggleTodo(id) {
